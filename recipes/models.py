92c3867c633913942b2cd44c8323d87375fa3ebf
Carmela Acevedo <kar288@gmail.com>
2016-04-28 10:34:01 +0200

    strings

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Strings(models.Model):
+    name = models.CharField(max_length=200)
     text = models.TextField()
 
 class Month(models.Model):
